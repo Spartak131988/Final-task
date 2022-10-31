da213ee0f6cb35a451b60c066ecb7e22ef95d3ec
alexandravdeenko <alex-avdeenko@yandex.ru>
2022-10-31 23:37:01 +0700

    Написал код для решения задачи

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿void Print(string[] array)
+{
+    Console.WriteLine(String.Join(' ', array));
+}
+
+string[] str = { "Hello", "2", "world", ":-)" };
+string[] result = new string[str.Length];
+int count = 0;
+for (int i = 0; i < str.Length; i++)
+{
+    if (str[i].Length <= 3)
+    {
+        result[count] = str[i];
+        count++;
+    }
+}
+Console.Write("Массив: ");
+Print(str);
+Console.Write("Новый массив: ");
+Print(result);
